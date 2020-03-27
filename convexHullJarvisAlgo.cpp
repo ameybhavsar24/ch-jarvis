f1826b7fc051af47cc69a3de2618623fcf5980fe
Amey Bhavsar <iamamey24@gmail.com>
2020-03-27 16:45:46 +0530

    Cleaned cpp code

--- convexHullJarvisAlgo.cpp
+++ convexHullJarvisAlgo.cpp
@@ -6,93 +6,71 @@ http://www.dcs.gla.ac.uk/~pat/52233/slides/Hull1x1.pdf
 */
 
 #include <bits/stdc++.h>
+
 using namespace std;
 
 struct Point {
-	int x;
-	int y;
+    int x;
+    int y;
 };
-
 
 //0: p,q and r are colinear
 //1: clockwise
 //2: counterclockwise
 
 int orientation(Point p, Point q, Point r) {
-	int val = (r.x - q.x)*(q.y - p.y) - (r.y - q.y)*(q.x - p.x);
-	if (val == 0) return 0;
-	return (val > 0)? 1: 2;
+    int val = (r.x - q.x) * (q.y - p.y) - (r.y - q.y) * (q.x - p.x);
+    if (val == 0) return 0;
+    return (val > 0) ? 1 : 2;
 }
 
+void convexHull(Point points[], int n) {
 
-void convexHull (Point points[], int n) {
+    //atleast three points are needed
+    if (n < 3) return;
 
-	//atleast three points are needed
-	if (n < 3) return;
+    //initialize result
+    vector < Point > hull;
 
-	//initialize result
-	vector<Point> hull;
+    //find the leftmost point
+    //we assume it to be point at index 0
+    int l = 0;
 
+    for (int i = 1; i < n; i++)
+        if (points[i].x < points[l].x)
+            l = i;
 
-	//find the leftmost point
-	//we assume it to be point at index 0
-	int l = 0;
+    int p = l, q;
 
-	for (int i=1; i<n; i++)
-		if (points[i].x < points[l].x)
-			l = i;
+    do {
+        hull.push_back(points[p]);
 
-	int p = l, q;
+        //store next point's index in a cyclic fashion
+        q = (p + 1) % n;
 
-	do {
-		hull.push_back(points[p]);
+        for (int i = 0; i < n; i++) {
+            //if point i is counterclockwise for more points than point q update q
 
-		//store next point's index in a cyclic fashion
-		q = (p+1) % n;
+            if (orientation(points[p], points[i], points[q]) == 2)
+                q = i;
+        }
 
-		for (int i=0; i<n; i++) {
-			//if point i is counterclockwise for more points than point q update q
+        p = q;
 
-			if (orientation(points[p],points[i],points[q]) == 2)
-				q = i;
-		}
+    } while (p != l);
 
-
-		p  = q;
-
-
-	} while(p != l);
-
-
-	for (int i=0; i< (int) hull.size(); i++) {
-		cout << "(" << hull[i].x << ", " << hull[i].y << ")\n";
-	}
+    for (int i = 0; i < (int) hull.size(); i++) {
+        cout << "(" << hull[i].x << ", " << hull[i].y << ")\n";
+    }
 
 }
-
-
-
 
 int main() {
 
-	//Point points[] = {{0, 3}, {2, 2}, {1, 1}, {2, 1}, {3, 0}, {0, 0}, {3, 3}};
+    Point points[]={{0,0},{0,1},{1,0},{1,1},{0,2},{1,2},{2,0},{2,1},{2,2},{3,0},{3,1},{3,2},{4,0},{4,1},{4,2},{5,0},{5,1}};
 
-	//int n;
-	//cout << "Enter number of points: ";
-	//cin >> n;
-	//Point points[n];
-	//cout << "Enter " << n << " points in the form x y";
-	//for (int i=0; i<n; i++) {
-		//int x,y;
-		//cin >> x >> y;
-		//points[i] = {x, y};
-	//}
-
-	Point points[] = {{0, 0}, {0, 1},{1, 0}, {1, 1}, {0, 2},{1,2},{2,0},{2,1},
-{2,2},{3,0},{3,1},{3, 2}, {4, 0},{4, 1},{4, 2}, {5, 0},{5,1}};
-
-    int n = sizeof(points)/sizeof(points[0]);
+    int n = sizeof(points) / sizeof(points[0]);
     convexHull(points, n);
 
-	return 0;
+    return 0;
 }
